0c6eb1c4075423d43f2d46e9d82f54acfdb38029
Reshma Rizvie <reshmarizvie@gmail.com>
2026-04-08 21:06:11 +0530

    initial project setup: spring boot and react

---
+++ src/main/java/lk/ac/sliit/it3030/smart_campus_project/SmartCampusProjectApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.it3030.smart_campus_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/sliit/it3030/smart_campus_project/SmartCampusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.it3030.smart_campus_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
